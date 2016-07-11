d00dc427ff4ca4dbab5b9fc15921c30213854538
Daichi Sakai <daisaru11@gmail.com>
2016-07-12 04:15:54 +0900

    fixed symlink checking conditions

--- linux/bootstrap.sh
+++ linux/bootstrap.sh
@@ -4,10 +4,10 @@ cd ${HOME}
 
 DOTFILES_DIR="./dotfiles/dotfiles"
 
-[ -L ${DOTFILES_DIR}/.vimrc ]         && ln -s ${DOTFILES_DIR}/.vimrc       .vimrc
-[ -L ${DOTFILES_DIR}/.vim   ]         && ln -s ${DOTFILES_DIR}/.vim         .vim
-[ -L ${DOTFILES_DIR}/.tmux.conf ]     && ln -s ${DOTFILES_DIR}/.tmux.conf   .tmux.conf
-[ -L ${DOTFILES_DIR}/.screenrc ]      && ln -s ${DOTFILES_DIR}/.screenrc    .screenrc
-[ -L ${DOTFILES_DIR}/.zshrc ]         && ln -s ${DOTFILES_DIR}/.zshrc       .zshrc
-[ -L ${DOTFILES_DIR}/.gitconfig ]     && ln -s ${DOTFILES_DIR}/.gitconfig   .gitconfig
+[ -L .vimrc ]         || ln -s ${DOTFILES_DIR}/.vimrc       .vimrc
+[ -L .vim   ]         || ln -s ${DOTFILES_DIR}/.vim         .vim
+[ -L .tmux.conf ]     || ln -s ${DOTFILES_DIR}/.tmux.conf   .tmux.conf
+[ -L .screenrc ]      || ln -s ${DOTFILES_DIR}/.screenrc    .screenrc
+[ -L .zshrc ]         || ln -s ${DOTFILES_DIR}/.zshrc       .zshrc
+[ -L .gitconfig ]     || ln -s ${DOTFILES_DIR}/.gitconfig   .gitconfig
 
